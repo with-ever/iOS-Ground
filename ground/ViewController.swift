107df895c24016f49716c75f0e12d58c7f6ee082
Gyeol <gyeol@madsq.net>
2019-08-16 21:35:05 +0900

    Init Project

---
+++ ground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ground
+//
+//  Created by HanGyeol on 16/08/2019.
+//  Copyright © 2019 WithEver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
