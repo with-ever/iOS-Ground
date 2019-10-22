f9ee3eb527e5dd58081d8ad0208cb1afc28fad65
TED <rlawldhsk@gmail.com>
2019-10-22 15:05:57 +0900

    테스트 코드 삭제

--- ground/src/ui/main/viewcontroller/MainViewController.swift
+++ ground/src/ui/main/viewcontroller/MainViewController.swift
@@ -13,7 +13,6 @@ class MainViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("test")
         // Do any additional setup after loading the view.
     }
     
